de7a2f9e96c1e30359e7e89062712285546c2ee1
rlim <rzlim08@gmail.com>
2018-02-22 17:09:40 -0800

    deleted files

--- @NiftiFS/ran_realignment.m
+++
@@ -1,6 +0,0 @@
-function ran_realignment(obj)
-% flag to show that slice_timing has been run. Also resets the
-% scan_strmatch to add the prefix
-obj.realignment = 1;
-obj.reset_scan_strmatch([obj.realignment_prefix obj.scan_strmatch]);
-end
--- @NiftiFS/ran_smoothing.m
+++
@@ -1,6 +0,0 @@
-function ran_smoothing(obj)
-% flag to show that smoothing has been run. Also resets the
-% scan_strmatch to add the prefix
-obj.smoothing = 1;
-obj.reset_scan_strmatch([obj.smoothing_prefix obj.scan_strmatch]);
-end
